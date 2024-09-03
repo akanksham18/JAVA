6b08d597218619d19899e9232e427b3b4517a118
Akanksha <akankshamali181@gmail.com>
2024-09-03 23:22:05 +0530

    first commit

---
+++ Litreal.java
@@ -0,0 +1,8 @@
+class Litreal{
+    public static void main(String a[]){
+        //int num=0b101;
+        //int num = 0x7E;
+        double num=100_00_000;
+        System.out.println(num);
+    }
+}
