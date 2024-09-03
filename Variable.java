0e24c44b1219310d0840ca884cecf76a78642c8a
Akanksha <akankshamali181@gmail.com>
2024-09-03 23:03:42 +0530

    first commit

--- Variable.java
+++ Variable.java
@@ -3,6 +3,19 @@ class Variable{
         int n=10;
         int m=20;
         int result=n+m;
+        double r=5.5;
+        float rf = 4.6f;
+        char c='k';
+        int a='k';
+        byte b=127;
+        short s=32767;
+        long l=2147483647L;
+        boolean bl = true;
+        
+        System.out.println(a);
+        System.out.println(c);
         System.out.println(result);
+        System.out.println(r);
+        System.out.println(rf);
     }
 }
